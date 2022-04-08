25e129d332d9fd3ad911c0c643c7dc0e47eb37d6
zzakirr <7nez6ws@code.edu.az>
2022-04-08 11:37:57 +0400

    C# file

---
+++ ClassLibrary/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        static int _no = 0;
+        public Student()
+        {
+            _no++;
+            No = _no;
+        }
+        public int No { get; set; }
+        public string Fullname { get; set; }
+        public Dictionary<string, int> Exams { get; set; } = new Dictionary<string, int>();
+        public void AddExam(string name,int point)
+        {
+            Exams.Add(name, point);
+        }
+        public int GetExamResult(string name)
+        {
+            foreach (var item in Exams)
+            {
+                if(item.Key == name)
+                {
+                    return item.Value;
+                }
+            }
+            return 0;
+        }
+        public double GetExamAvg()
+        {
+            int totalPoint = 0;
+            int count = 0;
+            foreach (var item in Exams)
+            {
+                totalPoint += item.Value ;
+                count++;
+            }
+            double avgPoint = totalPoint / count;
+            return avgPoint;
+        }
+    }
+}
---
+++ Practise_16/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using ClassLibrary;
+using System.Collections.Generic;
+namespace Practise_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>();
+            
+            string answer = "";
+            do
+            {
+                Console.WriteLine("1-Telebe elave et");
+                Console.WriteLine("2-Telebeye imtahan elave et");
+                Console.WriteLine("3-Telebenin bir imtahan balina bax");
+                Console.WriteLine("4-telebenin butun imtahanlarini goster");
+                Console.WriteLine("5-Telebenin imtahan ortalamasin goster");
+                Console.WriteLine("6-Imtahan sil");
+                Console.WriteLine("0-Proqrami bitir");
+                answer = Console.ReadLine();
+                
+                switch (answer)
+                {
+                    case "1":
+                        Console.WriteLine("Telebenin adi:");
+                        string fullname = Console.ReadLine();
+                        Student student = new Student();
+                        student.Fullname = fullname;
+                        studentList.Add(student);
+                        break;
+                    case "2":
+                        
+                        bool check = false;
+                        while(!check)
+                        {
+                            Console.WriteLine("Telebenin nomresi");
+                            int studentNo = int.Parse(Console.ReadLine());
+                            foreach (var item in studentList)
+                            {
+                                if (item.No == studentNo)
+                                {
+                                    Console.WriteLine("imtahan adi:");
+                                    string examName = Console.ReadLine();
+                                    Console.WriteLine("Imtahan bali:");
+                                    int examPoint = int.Parse(Console.ReadLine());
+                                    item.AddExam(examName, examPoint);
+                                    check = true;
+                                }
+
+                            }
+                            if (!check)
+                            {
+                                Console.WriteLine("Bu nomrede telebe yoxdu");
+                                break;
+                            }
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Telebenin No-su:");
+                        int no = int.Parse(Console.ReadLine());
+                        bool check1 = false;
+                        foreach (var item in studentList)
+                        {
+                            if (item.No == no)
+                            {
+                                Console.WriteLine("neticesine baxmaq istediyiniz imtahanin adi:");
+                                string exName = Console.ReadLine();
+                                Console.WriteLine(item.GetExamResult(exName)); 
+                                check1 = true;
+                            }
+
+                        }
+                        if (!check1)
+                        {
+                            Console.WriteLine("Bu nomrede telebe yoxdu");
+                            break;
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("imtahanlarina baxmaq istediyiniz telebenin No-su");
+                        int no4 = int.Parse(Console.ReadLine());
+                        bool check2 = false;
+                        foreach (var item in studentList)
+                        {
+                            if (item.No == no4)
+                            {
+                                foreach (var ex in item.Exams)
+                                {
+                                    Console.WriteLine(ex);
+                                }
+                                check2 = true;
+                            }
+
+                        }
+                        if (!check2)
+                        {
+                            Console.WriteLine("Bu nomrede telebe yoxdu");
+                            break;
+                        }
+                        break;
+                    case "5":
+                        Console.WriteLine("imtahanlarinin ortalama balina baxmaq istediyiniz telebenin No-su");
+                        int no5 = int.Parse(Console.ReadLine());
+                        bool check3 = false;
+                        foreach (var item in studentList)
+                        {
+                            if (item.No == no5)
+                            {
+                                Console.WriteLine(item.GetExamAvg()); 
+                                check2 = true;
+                            }
+
+                        }
+                        if (!check3)
+                        {
+                            Console.WriteLine("Bu nomrede telebe yoxdu");
+                            break;
+                        }
+                        break;
+                    case "6":
+                        Console.WriteLine("imtahanini silmey istediyiniz telebennin No-su");
+                        int no6 = int.Parse(Console.ReadLine());
+                        bool check4 = false;
+                        foreach (var item in studentList)
+                        {
+                            if (item.No == no6)
+                            {
+                                Console.WriteLine("silmey istediyiniz imtahanin adi:");
+                                string exname = Console.ReadLine();
+                                item.Exams.Remove(exname);
+                                check2 = true;
+                            }
+
+                        }
+                        if (!check4)
+                        {
+                            Console.WriteLine("Bu nomrede telebe yoxdu");
+                            break;
+                        }
+                        
+                        break;
+                    default:
+                        break;
+                }
+            } while (answer!="0");
+        }
+    }
+}
